fc41f0cef68cfba1c4ea79aa3f07e26f1cf60025
Andre <sotfs@posteo.de>
2022-09-16 21:15:00 +0200

    script to download deps

---
+++ get_deps_x11_64.sh
@@ -0,0 +1,15 @@
+mkdir -p addons/pythonscript/x11-64
+wget https://github.com/touilleMan/godot-python-assetlib-repo/archive/9c36f5b82d890c2b6f6ba3dea65ee3381c35c991.zip
+unzip 9c36f5b82d890c2b6f6ba3dea65ee3381c35c991.zip
+mv -v godot-python-assetlib-repo-9c36f5b82d890c2b6f6ba3dea65ee3381c35c991/pythonscript.gdnlib .
+mv -v godot-python-assetlib-repo-9c36f5b82d890c2b6f6ba3dea65ee3381c35c991/*.txt addons/pythonscript 
+mv -v godot-python-assetlib-repo-9c36f5b82d890c2b6f6ba3dea65ee3381c35c991/addons/pythonscript/x11-64/* addons/pythonscript/x11-64/
+rm -rv godot-python-assetlib-repo-9c36f5b82d890c2b6f6ba3dea65ee3381c35c991
+rm -v 9c36f5b82d890c2b6f6ba3dea65ee3381c35c991.zip
+
+touch addons/.gdignore
+cd addons/pythonscript/x11-64/
+chmod +x bin/python3.8
+bin/python3.8 -m ensurepip
+./bin/python3.8 -m pip install -e git+https://github.com/s0tfs/stable-diffusion@main#egg=latent-diffusion git+https://github.com/CompVis/taming-transformers.git@master#egg=taming-transformers git+https://github.com/openai/CLIP.git@main#egg=clip
+./x11-64/bin/python3.8 -m pip install omegaconf einops transformers pytorch-lightning pandas kornia scipy
