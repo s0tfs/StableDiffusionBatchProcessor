2a88acba23cebb7a871c67b84f47bda2c2082fc1
Andre <sotfs@posteo.de>
2022-09-16 22:41:50 +0200

    fix get_deps_x11_64.sh

--- get_deps_x11_64.sh
+++ get_deps_x11_64.sh
@@ -11,5 +11,12 @@ touch addons/.gdignore
 cd addons/pythonscript/x11-64/
 chmod +x bin/python3.8
 bin/python3.8 -m ensurepip
-./bin/python3.8 -m pip install -e git+https://github.com/s0tfs/stable-diffusion@main#egg=latent-diffusion git+https://github.com/CompVis/taming-transformers.git@master#egg=taming-transformers git+https://github.com/openai/CLIP.git@main#egg=clip
-./x11-64/bin/python3.8 -m pip install omegaconf einops transformers pytorch-lightning pandas kornia scipy
+./bin/python3.8 -m pip install -e git+https://github.com/s0tfs/stable-diffusion@main#egg=latent-diffusion 
+./bin/python3.8 -m pip install -e git+https://github.com/CompVis/taming-transformers.git@master#egg=taming-transformers 
+./bin/python3.8 -m pip install -e git+https://github.com/openai/CLIP.git@main#egg=clip
+./bin/python3.8 -m pip install omegaconf einops transformers pytorch-lightning pandas kornia scipy
+
+ rm -rfv src/stable-diffusion/.git
+ rm -rfv src/clip/.git
+ rm -rfv src/taming_transformers/.git
+ find . -type f -name '*.jpg' -delete
